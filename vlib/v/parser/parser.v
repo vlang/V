8963caf5639babfdd75607a3b3b13ceee4321dac
Florian Blasius <florian.blasius@ergosign.de>
2021-06-08 10:53:19 +0200

    add enum_decls map to table

--- vlib/v/parser/parser.v
+++ vlib/v/parser/parser.v
@@ -3011,7 +3011,8 @@ fn (mut p Parser) enum_decl() ast.EnumDecl {
 		p.error_with_pos('cannot register enum `$name`, another type with this name exists',
 			end_pos)
 	}
-	return ast.EnumDecl{
+
+	enum_decl := ast.EnumDecl{
 		name: name
 		is_pub: is_pub
 		is_flag: is_flag
@@ -3021,6 +3022,10 @@ fn (mut p Parser) enum_decl() ast.EnumDecl {
 		attrs: p.attrs
 		comments: enum_decl_comments
 	}
+
+	p.table.register_enum_decl(enum_decl)
+
+	return enum_decl
 }
 
 fn (mut p Parser) type_decl() ast.TypeDecl {
